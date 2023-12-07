afc20822c818ce9df9c0bfd1cb59e6711e2c245b
nagarajghadi <143261867+nagarajghadi@users.noreply.github.com>
2023-12-07 07:22:26 +0000

    datatypes

---
+++ firstjavascript/datatypes.js
@@ -0,0 +1,36 @@
+"use strict"; 
+// treat alll JS code as newer version
+
+// alert("hello")    we are using node js not browser 
+
+console.log(3 + 3);
+// code readability should be high
+
+// Datatypes
+
+// 1)number => 2 to power 53
+// bigint
+
+// string => "", ''
+
+// boolean => true/false 
+
+// null => standalone value 
+
+// undefined =>
+
+// sybol => unique 
+
+let name = "Raj"
+let age = 23 
+let isloggedIn = false
+
+
+// object 
+//  console.log(typeof undefined)  // undefined 
+// console.log(typeof null);     // object
+
+
+ let stateCity = null;
+ let state;
+ console.log(typeof stateCity, state);
