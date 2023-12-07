408fe668abda6424d8c17f31fbcd002309f8b032
nagarajghadi <143261867+nagarajghadi@users.noreply.github.com>
2023-12-07 08:02:30 +0000

    conversion of datatypes

---
+++ firstjavascript/conversionOperation.js
@@ -0,0 +1,28 @@
+// let score = "33fjhuk" 
+
+// let score = null // 0
+
+// let score = undefined  //NAN (Not A Number)
+
+// let score = true 
+
+// 1 => truw; 0 => false
+//  "" => false
+// "Rakesh" => true
+
+let isLoggedIn = "Rakesh"
+
+let booleanLoggedIn = Boolean(isLoggedIn)
+console.log(booleanLoggedIn);
+
+
+
+
+// console.log(typeof score);
+
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
